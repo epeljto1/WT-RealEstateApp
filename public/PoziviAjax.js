80ec5460f4cda269c1abeb35655c16b382fe25ff
epeljto1 <epeljto1@etf.unsa.ba>
2023-12-26 23:18:51 +0100

    Dodane funkcije impl_putKorisnik i impl_postUpit

--- public/PoziviAjax.js
+++ public/PoziviAjax.js
@@ -20,11 +20,55 @@ const PoziviAjax = (() => {
         xhttp.send();  
     }
 
+    function impl_putKorisnik(noviPodaci, fnCallback)
+    {
+        let xhttp = new XMLHttpRequest();
+
+        xhttp.open("PUT","http://localhost:3000/korisnik",true);
+        xhttp.onreadystatechange = function()
+        {
+            if(xhttp.readyState==4 && xhttp.status==200)
+            {
+                const data = JSON.parse(xhttp.responseText);
+                fnCallback(null,data);
+                updateMeni(true);
+            }
+            else if(xhttp.status==4){
+                const error = JSON.parse(xhttp.responseText);
+                fnCallback(error,null);
+            }
+        }
+        xhttp.send(JSON.stringify(noviPodaci));
+    }
+
+    function impl_postUpit(nekretnina_id, tekst_upita, fnCallback)
+    {
+        let xhttp = new XMLHttpRequest();
+
+        xhttp.open("POST","http://localhost:3000/upit",true);
+        xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhttp.onreadystatechange = function()
+        {
+            if(xhttp.readyState==4 && xhttp.status==200)
+            {
+                const data = JSON.parse(xhttp.responseText);
+                fnCallback(null,data);
+            }
+            else if (xhttp.readyState==4)
+            {
+                const error = JSON.parse(xhttp.responseText);
+                fnCallback(error,null);
+            }
+        }
+        xhttp.send("nekretnina_id="+nekretnina_id+"&tekst_upita="+tekst_upita);
+    }
+
     function impl_getNekretnine(fnCallback)
     {
         let xhttp = new XMLHttpRequest();
 
         xhttp.open("GET","http://localhost:3000/nekretnine",true);
+        
         xhttp.onreadystatechange = function()
         {
             if(xhttp.readyState==4 && xhttp.status==200)
@@ -109,6 +153,8 @@ const PoziviAjax = (() => {
         postLogin: impl_postLogin,
         postLogout: impl_postLogout,
         getKorisnik: impl_getKorisnik,
+        putKorisnik: impl_putKorisnik,
+        postUpit: impl_postUpit,
         getNekretnine: impl_getNekretnine
     };
 })();
